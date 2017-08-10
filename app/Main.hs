dd49b26cf2b1e56810d0d4ddaa57e23f7091eb66
Michael Maier <maier1michael@gmail.com>
2017-08-10 15:57:30 +0200

    port chapter 3 "Displaying Graphics" from the tutorial

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
@@ -6,12 +7,27 @@ import Control.Monad (when, unless)
 import Control.Concurrent (threadDelay)
 import Data.Maybe (mapMaybe)
 import Foreign.C.Types (CInt)
-import SDL.Vect (V2(..), V4(..))
+import SDL.Vect (Point(..), V2(..), V4(..))
 import SDL (($=))
+import GHC.Word (Word8(..))
 import qualified SDL
+import qualified SDL.Image
+import qualified SDL.Raw
+
+import Paths_sdl2_jump_and_run (getDataFileName)
 
 screenWidth, screenHeight :: CInt
 (screenWidth, screenHeight) = (1280, 720)
+
+type Color = V4 Word8
+type Vector2d = V2 CInt
+type Point2d = Point V2 CInt
+
+p2 :: CInt -> CInt -> Point2d
+p2 x y = P $ V2 x y
+
+rgb :: Word8 -> Word8 -> Word8 -> Color
+rgb r g b = V4 r g b maxBound
 
 data Input
   = None
@@ -21,6 +37,27 @@ data Input
   | Restart
   | Quit
   deriving (Eq)
+
+data Player = Player
+  { texture :: !SDL.Texture
+  , position :: !Point2d
+  , velocity :: !Vector2d
+  }
+
+createPlayer :: SDL.Texture -> Player
+createPlayer t = Player
+  { texture = t
+  , position = p2 170 500
+  , velocity = V2 0 0
+  }
+
+restartPlayer :: Player -> Player
+restartPlayer p = createPlayer $ texture p
+
+data Game = Game
+  { player :: !Player
+  , camera :: !Vector2d
+  }
 
 toInput :: SDL.Keycode -> Input
 toInput = \case
@@ -37,6 +74,81 @@ isPressedKey eventData = SDL.Pressed == SDL.keyboardEventKeyMotion eventData
 toPressedKey :: SDL.EventPayload -> Maybe SDL.Keycode
 toPressedKey (SDL.KeyboardEvent e) | isPressedKey e = Just $ SDL.keysymKeycode $ SDL.keyboardEventKeysym e
 toPressedKey _ = Nothing
+
+loadTexture :: SDL.Renderer -> FilePath -> IO SDL.Texture
+loadTexture renderer filePath =
+  getDataFileName filePath >>= SDL.Image.loadTexture renderer
+
+data CopyExData = CopyExData
+  { source :: SDL.Rectangle CInt
+  , destination :: SDL.Rectangle CInt
+  , flips :: V2 Bool
+  }
+
+flipNone = V2 False False
+flipHorizontal = V2 False True
+flipVertical = V2 True False
+
+renderTee :: SDL.Renderer -> SDL.Texture -> Point2d -> IO [()]
+renderTee renderer texture position =
+  let
+    (P (V2 x y)) = position
+    backFeetShadow = CopyExData
+      { source = SDL.Rectangle (p2 192 64) (V2 64 32)
+      , destination = SDL.Rectangle (p2 (x - 60) y) (V2 96 48)
+      , flips = flipNone
+      }
+    bodyShadow = CopyExData
+      { source = SDL.Rectangle (p2 96 0) (V2 96 96)
+      , destination = SDL.Rectangle (p2 (x - 48) (y - 48)) (V2 96 96)
+      , flips = flipNone
+      }
+    frontFeetShadow = CopyExData
+      { source = SDL.Rectangle (p2 192 64) (V2 64 32)
+      , destination = SDL.Rectangle (p2 (x - 36) y) (V2 96 48)
+      , flips = flipNone
+      }
+    backFeet = CopyExData
+      { source = SDL.Rectangle (p2 192 32) (V2 64 32)
+      , destination = SDL.Rectangle (p2 (x - 60) y) (V2 96 48)
+      , flips = flipNone
+      }
+    body = CopyExData
+      { source = SDL.Rectangle (p2  0 0) (V2 96 96)
+      , destination = SDL.Rectangle (p2 (x - 48) (y - 48)) (V2 96 96)
+      , flips = flipNone
+      }
+    frontFeet = CopyExData
+      { source = SDL.Rectangle (p2 192 32) (V2 64 32)
+      , destination = SDL.Rectangle (p2 (x - 36) y) (V2 96 48)
+      , flips = flipNone
+      }
+    leftEye = CopyExData
+      { source = SDL.Rectangle (p2 64 96) (V2 32 32)
+      , destination = SDL.Rectangle (p2 (x - 18) (y - 21)) (V2 36 36)
+      , flips = flipNone
+      }
+    rightEye = CopyExData
+      { source = SDL.Rectangle (p2 64 96) (V2 32 32)
+      , destination = SDL.Rectangle (p2  (x - 6) (y - 21)) (V2 36 36)
+      , flips = flipHorizontal
+      }
+    bodyParts =
+      [ backFeetShadow
+      , bodyShadow
+      , frontFeetShadow
+      , backFeet
+      , body
+      , frontFeet
+      , leftEye
+      , rightEye
+      ]
+    angle = 0.0
+    rotation = Nothing
+    copy bodyPart@(CopyExData {source, destination, flips}) =
+      SDL.copyEx renderer texture (Just source) (Just destination) angle rotation flips
+  in
+    mapM copy bodyParts
 
 main :: IO ()
 main = do
@@ -64,15 +176,21 @@ main = do
           { SDL.rendererType = SDL.AcceleratedVSyncRenderer
           , SDL.rendererTargetTexture = True
           }
-  SDL.rendererDrawColor renderer $= V4 110 132 174 maxBound
+  SDL.rendererDrawColor renderer $= rgb 110 132 174
 
+  playerTexture <- loadTexture renderer "assets/player.png"
   let
+    game = Game
+      { player = createPlayer playerTexture
+      , camera = V2 0 0
+      }
     loop = do
       events <- map SDL.eventPayload <$> SDL.pollEvents
       let pressedKeys = mapMaybe toPressedKey events
       let inputs = map toInput pressedKeys
 
       SDL.clear renderer
+      renderTee renderer playerTexture ((position $ player game) - (P $ camera game))
       SDL.present renderer
 
       let quit = (SDL.QuitEvent `elem` events) || (Quit `elem` inputs)
